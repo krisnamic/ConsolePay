6a76db23af6a721e7490f9fa05fd7637b4a65b0f
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-05-15 14:46:43 +0700

    test preset

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
@@ -13,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([UsersTableSeeder::class]);
     }
 }
