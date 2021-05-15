16046c81738fdab2a7d2792e9ba9432dd83f784c
steven278 <darkavenger347@gmail.com>
2021-05-15 13:50:45 +0700

    bikin migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('noTelepon');
+            $table->string('alamat');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_05_15_062325_create_pesanan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePesananTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesanan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->date('tanggalPemesanan');
+            $table->integer('jumlahHari');
+            $table->integer('hargaTotal');
+            $table->integer('statusPemesanan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesanan');
+    }
+}
