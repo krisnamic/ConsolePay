7104512083923e27b987ccd14efb54743222e649
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-05-18 19:45:59 +0700

    navbar

--- resources/views/Template/navbar.blade.php
+++ resources/views/Template/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
-    <ul class="navbar-nav">
+    <!-- <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
@@ -10,7 +10,26 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li>
-    </ul>
+    </ul> -->
+
+    
+
+    @if (Route::has('login'))
+            @auth
+                <li class="nav-item d-none d-sm-inline-block">
+                  <a href="{{ url('/home') }}" class="nav-link">Home</a>
+                </li>
+            @else
+                <li class="nav-item d-none d-sm-inline-block">
+                  <a href="{{ route('login') }}" class="nav-link">Login</a>
+                </li>
+                @if (Route::has('register'))
+                    <li class="nav-item d-none d-sm-inline-block">
+                      <a href="{{ route('register') }}" class="nav-link">Login</a>
+                    </li>
+                @endif
+            @endauth
+    @endif
 
     <!-- SEARCH FORM -->
     <form class="form-inline ml-3">
