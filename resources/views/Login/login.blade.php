c0c0284b91a5af0c9cc4c028df22141caf496838
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-05-21 11:10:45 +0700

    improved login register

--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -23,120 +23,92 @@
   <link href="https://fonts.googleapis.com/css?family=Kanit:300,300i,400,400i,600,600i,700,700i|Varela:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 </head>
 
-<body class="hold-transition login-page bg-img">
+<body class="hold-transition login-page overflow-hidden">
 
   <!-- Head -->
   @include('Template/head')
 
-  <div class="login-box">
-    <!-- ConsolePay Logo -->
-    <div class="login-logo">
-      <object class="home-login" data="img/consolepay/consolepay-horizontal.svg" type="image/svg+xml"></object>
-    </div>
-    <!-- Login Card -->
-    <div class="card">
-      <div class="card-body login-card-body">
-        <h5>Sign in to start your session</h5>
-        <form action="{{ route('postlogin') }}" method="post">
-          {{csrf_field()}}
-          <div class="input-group mb-3">
-            <input type="text" class="form-control" name="email" placeholder="Email">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
-              </div>
-            </div>
-          </div>
-          <p style="font-size:12px; color:red;">@error('email')
-            {{ $message }}
-            @enderror
-          </p>
-          <div class="input-group mb-3">
-            <input type="password" class="form-control" name="password" placeholder="Password">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
-              </div>
-            </div>
-          </div>
-          <p style="font-size:12px; color:red;">@error('password')
-            {{ $message }}
-            @enderror
-          </p>
-          @if(Session::has('error'))
-          <p class="font-size:12px; color:red;">{{ Session::get('error') }}</p>
-          @endif
-          <div class="row">
-            <!-- <div class="col-8">
-              <div class="icheck-primary">
-              </div>
-            </div> -->
+  <div class="bg-container">
 
-            <div class="d-flex align-items-center mb-0">
-                <a href="{{route('register')}}" class="text-center" style="vertical-align:middle;">I don't have an account</a>
-            </div>
-            
-
-            <!-- captcha -->
-            <!-- /.col -->
-            <div class="ml-auto">
-              <button type="submit" id="login-btn" class="btn btn-primary btn-block">Sign In</button>
-            </div>
-            <!-- /.col -->
-
-            <br>
-
-            
-          </div>
-        </form>
-
-        <div class="mb-0">
-          <a href="/" class="text-center" style="vertical-align:middle;">Back to main page</a>
-        </div>
-
-        <!-- <p class="mb-1">
-        </p> -->
-        
+    <div style="z-index: 1080;">
+      <div class="login-box">
+      <!-- ConsolePay Logo -->
+      <div class="login-logo">
+        <object class="home-login" data="img/consolepay/consolepay-horizontal.svg" type="image/svg+xml"></object>
       </div>
-      <!-- /.login-card-body -->
+      <!-- Login Card -->
+      <div class="card shadow">
+        <div class="card-body login-card-body">
+          <h5>Sign in to start your session</h5>
+          <form action="{{ route('postlogin') }}" method="post">
+            {{csrf_field()}}
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" name="email" placeholder="Email">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope"></span>
+                </div>
+              </div>
+            </div>
+            <p style="font-size:12px; color:red;">@error('email')
+              {{ $message }}
+              @enderror
+            </p>
+            <div class="input-group mb-3">
+              <input type="password" class="form-control" name="password" placeholder="Password">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+            <p style="font-size:12px; color:red;">@error('password')
+              {{ $message }}
+              @enderror
+            </p>
+            @if(Session::has('error'))
+            <p class="font-size:12px; color:red;">{{ Session::get('error') }}</p>
+            @endif
+            <div class="row">
+              <!-- <div class="col-8">
+                <div class="icheck-primary">
+                </div>
+              </div> -->
+
+              <div class="d-flex flex-column justify-content-start align-items-left mb-0" style="">
+                <a href="{{route('register')}}" class="text-left" style="vertical-align:middle;">> I don't have an account</a>
+                <a href="/" class="text-left" style="vertical-align:middle;">> Back to main page</a>
+              </div>
+              
+
+              <!-- captcha -->
+              <!-- /.col -->
+              <div class="ml-auto">
+                <button type="submit" id="login-btn" class="btn btn-primary btn-block">Sign In</button>
+              </div>
+              <!-- /.col -->
+
+              <br>
+
+              
+            </div>
+          </form>
+
+          <!-- <p class="mb-1">
+          </p> -->
+          
+        </div>
+        <!-- /.login-card-body -->
+      </div>
     </div>
+    <!-- /.login-box -->
+
+    @include('Template/script')
+
+    </div>
+    <div class="sliding-background" style="position: relative !important; z-index: -1080;"></div>
   </div>
-  <!-- /.login-box -->
 
-  @include('Template/script')
-  <!-- Vendor JS Files -->
-  <!-- <script src="../../../vendor/"></script>
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
-
-  <script>
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    // (function() {
-    //   'use strict';
-    //   window.addEventListener('load', function() {
-    //     document.getElementById("login-btn").disabled = true;
-    //     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    //     var forms = document.getElementsByClassName('needs-validation');
-    //     // Loop over them and prevent submission
-    //     var validation = Array.prototype.filter.call(forms, function(form) {
-    //       form.addEventListener('submit', function(event) {
-    //         if (form.checkValidity() === false) {
-    //           event.preventDefault();
-    //           event.stopPropagation();
-    //         }
-    //         form.classList.add('was-validated');
-    //       }, false);
-    //     });
-    //   }, false);
-    // })();
-  </script>
-  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script> -->
-  <!-- <script src="../../js/captcha.js"></script> -->
-  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/venobox/1.9.3/venobox.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script> -->
 </body>
 
 </html>
--- resources/views/Login/register.blade.php
+++ resources/views/Login/register.blade.php
@@ -20,119 +20,131 @@
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 
-<body class="hold-transition register-page bg-img">
+<body class="hold-transition register-page overflow-hidden">
 
   <!-- Head -->
   @include('Template/head')
 
-  <div class="register-box">
-    <!-- ConsolePay Logo -->
-    <div class="login-logo">
-      <object class="home-login" data="img/consolepay/consolepay-horizontal.svg" type="image/svg+xml"></object>
-    </div>
+  <div class="bg-container">
 
-    <!-- Register Card -->
-    <div class="card">
-      <div class="card-body register-card-body">
-        <h5>Register a new membership</h5>
-
-        <form action="{{route('savedata')}}" method="post">
-          {{csrf_field()}}
-          @csrf
-          <div class="card-body">
-            <input type="hidden" name="role" value="user" \>
-            <div class="input-group mb-3">
-              <input type="text" class="form-control" name="name" placeholder="Full name">
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-user"></span>
-                </div>
-              </div>
-            </div>
-            <p style="font-size:12px; color:red;">@error('name')
-              {{ $message }}
-              @enderror
-            </p>
-            <div class="input-group mb-3">
-              <input type="text" class="form-control" name="email" placeholder="Email">
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-envelope"></span>
-                </div>
-              </div>
-            </div>
-            <p style="font-size:12px; color:red;">@error('email')
-              {{ $message }}
-              @enderror
-            </p>
-            <div class="input-group mb-3">
-              <input type="text" class="form-control" name="noTelepon" placeholder="Phone number">
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-phone"></span>
-                </div>
-              </div>
-            </div>
-            <p style="font-size:12px; color:red;">@error('noTelepon')
-              {{ $message }}
-              @enderror
-            </p>
-            <div class="input-group mb-3">
-              <input type="text" class="form-control" name="alamat" placeholder="Address">
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-home"></span>
-                </div>
-              </div>
-            </div>
-            <p style="font-size:12px; color:red;">@error('alamat')
-              {{ $message }}
-              @enderror
-            </p>
-            <div class="input-group mb-3">
-              <input type="password" class="form-control" name="password" placeholder="Password">
-              <div class="input-group-append">
-                <div class="input-group-text">
-                  <span class="fas fa-lock"></span>
-                </div>
-              </div>
-            </div>
-            <p style="font-size:12px; color:red;">@error('password')
-              {{ $message }}
-              @enderror
-            </p>
-            <div class="input-group mb-3">
-            </div>
-            <div class="row d-flex justify-content-between">
-              <!-- <div class="col-8">
-                <div class="icheck-primary">
-                </div>
-              </div> -->
-              <div class="d-flex align-items-center mb-0">
-                <a href="{{route('login')}}" class="text-center" style="vertical-align:middle;">I already have an account</a>
-              </div>
-              
-              <!-- captcha -->
-              <!-- /.col -->
-              <div class="ml-auto">
-                <button type="submit" id="login-btn" class="btn btn-primary btn-block">Register</button>
-              </div>
-
-              <!-- /.col -->
-              <!-- <div class="col-4">
-                <button type="submit" class="btn btn-primary btn-block">Register</button>
-              </div> -->
-              <!-- /.col -->
-            </div>
-        </form>
-
-
-        <!-- <a href="{{route('login')}}" class="text-center">I already have an account</a> -->
+    <div style="z-index: 1080;">
+    <div  class="register-box">
+      <!-- ConsolePay Logo -->
+      <div class="login-logo">
+        <object class="home-login" data="img/consolepay/consolepay-horizontal.svg" type="image/svg+xml"></object>
       </div>
-      <!-- /.form-box -->
-    </div><!-- /.card -->
+
+      <!-- Register Card -->
+      <div class="card shadow">
+        <div class="card-body register-card-body">
+          <h5>Register a new membership</h5>
+
+          <form action="{{route('savedata')}}" method="post">
+            {{csrf_field()}}
+            @csrf
+            <div class="card-body">
+              <input type="hidden" name="role" value="user" \>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" name="name" placeholder="Full name">
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-user"></span>
+                  </div>
+                </div>
+              </div>
+              <p style="font-size:12px; color:red;">@error('name')
+                {{ $message }}
+                @enderror
+              </p>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" name="email" placeholder="Email">
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-envelope"></span>
+                  </div>
+                </div>
+              </div>
+              <p style="font-size:12px; color:red;">@error('email')
+                {{ $message }}
+                @enderror
+              </p>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" name="noTelepon" placeholder="Phone number">
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-phone"></span>
+                  </div>
+                </div>
+              </div>
+              <p style="font-size:12px; color:red;">@error('noTelepon')
+                {{ $message }}
+                @enderror
+              </p>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" name="alamat" placeholder="Address">
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-home"></span>
+                  </div>
+                </div>
+              </div>
+              <p style="font-size:12px; color:red;">@error('alamat')
+                {{ $message }}
+                @enderror
+              </p>
+              <div class="input-group mb-3">
+                <input type="password" class="form-control" name="password" placeholder="Password">
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
+                  </div>
+                </div>
+              </div>
+              <p style="font-size:12px; color:red;">@error('password')
+                {{ $message }}
+                @enderror
+              </p>
+              <div class="input-group mb-3">
+              </div>
+              <div class="row d-flex justify-content-between">
+                <!-- <div class="col-8">
+                  <div class="icheck-primary">
+                  </div>
+                </div> -->
+                <div class="d-flex flex-column mb-0">
+                  <a href="{{route('login')}}" class="text-left" style="vertical-align:middle;">> I already have an account</a>
+                  <a href="/" class="text-left" style="vertical-align:middle;">> Back to main page</a>
+                </div>
+                
+                <!-- captcha -->
+                <!-- /.col -->
+                <div class="ml-auto">
+                  <button type="submit" id="login-btn" class="btn btn-primary btn-block">Register</button>
+                </div>
+
+                <!-- /.col -->
+                <!-- <div class="col-4">
+                  <button type="submit" class="btn btn-primary btn-block">Register</button>
+                </div> -->
+                <!-- /.col -->
+              </div>
+          </form>
+
+
+          <!-- <a href="{{route('login')}}" class="text-center">I already have an account</a> -->
+        </div>
+        <!-- /.form-box -->
+      </div><!-- /.card -->
+    </div>
+    <!-- /.register-box -->
+    </div>
+    
+  <div>
+    
   </div>
-  <!-- /.register-box -->
+  
+  <div class="sliding-background" style="position: relative !important; z-index: -1080;"></div>
+  </div>
 
   @include('Template/script')
 </body>
