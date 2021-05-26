6dae85054cdf7fea0e5092ab59c7484993552105
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-05-26 21:46:29 +0700

    cuma nambahin page title doang

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>ConsolePay</title>
+        <title>ConsolePay | Admin</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
--- resources/views/user/shoppingCart.blade.php
+++ resources/views/user/shoppingCart.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ConsolePay | Shopping Cart</title>
     @include('Template/head')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
