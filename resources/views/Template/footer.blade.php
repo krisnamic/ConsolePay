a34173f4f7dd3eb075cc3aeab7ab8b6b3fdb99bc
jonathan <jonathan.franzeli@student.umn.ac.id>
2021-05-30 21:31:44 +0700

    lupa commit footer kemarin

--- resources/views/Template/footer.blade.php
+++ resources/views/Template/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer footer">
   <!-- To the right -->
   <div class="float-right d-none d-sm-inline">
-    Hello world!
+    "You Play, You Pay, We Happy!"
   </div>
   <!-- Default to the left -->
   <strong>Copyright &copy; 2021 <a href="https://sushipay-pemweb.xyz">consolepay.xyz</a></strong> All rights reserved.
