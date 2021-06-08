da4c7dde7b908dd102c5bd83bb8b9fe1ef0c295f
Michael KC <cahyadi.michael.mkc24@gmail.com>
2021-06-08 13:29:17 +0700

    Revert "."
    This reverts commit db54baecd15b944a380e1abf1db5eceee05595a1.

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'consolepay.herokuapp.com'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
