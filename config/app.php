f725d2820aa81c30bf54e1eb993c3b3f235b21fd
Michael KC <cahyadi.michael.mkc24@gmail.com>
2021-06-08 13:29:43 +0700

    Revert "change url"
    This reverts commit ab9a4bfeddc81c799d59355c89fb2aa35329f736.

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://consolepay.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
