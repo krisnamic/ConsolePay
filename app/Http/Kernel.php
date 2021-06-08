6a965bb5725aa1db1ce8ea2dde15116d10fddd07
Michael KC <cahyadi.michael.mkc24@gmail.com>
2021-06-08 17:07:16 +0700

    add cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'checkrole' => \App\Http\Middleware\CheckRole::class,
+        'cors' => \App\Http\Middleware\Cors::class, 
     ];
 }
